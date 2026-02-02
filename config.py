e644843ed447d1c00bce065d18a427331cccc398
dlinsalata-gif <dlinsalata@shipleyschool.org>
2026-02-01 22:14:50 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# Stable serial path (use by-id if available)
+LIDAR_PORT = "/dev/ttyUSB0"
+
+# Virtual line angles (degrees)
+ENTRY_LINE = 85
+EXIT_LINE = 275
+
+# Distance threshold (mm)
+TRIGGER_DISTANCE = 1800
+
+# Angle tolerance
+ANGLE_WINDOW = 4
