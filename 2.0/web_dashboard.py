103fda5a8c4d71baf24d3104196e49660c99d076
dlinsalata-gif <dlinsalata@shipleyschool.org>
2026-02-03 09:38:15 -0500

    Update web_dashboard.py

--- 2.0/web_dashboard.py
+++ 2.0/web_dashboard.py
@@ -17,3 +17,34 @@ def index():
         event=event,
         event_time=event_time
     )
+
+
+
+from flask import Flask, render_template, jsonify
+from lidar_counter import car_count, last_event, last_event_time, lock
+from config import MAX_SPOTS
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/status")
+def status():
+    with lock:
+        open_spots = MAX_SPOTS - car_count
+        if open_spots < 0:
+            open_spots = 0
+
+        return jsonify(
+            open_spots=open_spots,
+            event=last_event,
+            event_time=last_event_time
+        )
+
+
+def start_web():
+    app.run(host="0.0.0.0", port=5000, threaded=True)
